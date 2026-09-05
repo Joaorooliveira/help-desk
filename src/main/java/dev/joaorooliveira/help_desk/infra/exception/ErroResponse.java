348f2ad6895c7528c0347cd16ba4065b1272dfe1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-05 18:07:27 -0300

    feat: adiciona erro response

--- src/main/java/dev/joaorooliveira/help_desk/infra/exception/ErroResponse.java
+++ src/main/java/dev/joaorooliveira/help_desk/infra/exception/ErroResponse.java
@@ -1,4 +1,12 @@
 package dev.joaorooliveira.help_desk.infra.exception;
 
-public record ErroResponse() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErroResponse(
+        LocalDateTime timestamp,
+        Integer status,
+        String erro,
+        List<String> mensagens
+) {
 }
