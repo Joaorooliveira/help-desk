ba732729458eb9e18c26028515e03d5717511063
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-06 20:15:05 -0300

    feat: adiciona classe tecnico

--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/Tecnico.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/Tecnico.java
@@ -1,4 +1,48 @@
 package dev.joaorooliveira.help_desk.domain.tecnico;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tecnico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Tecnico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 150)
+    private String nome;
+
+    @Column(nullable = false,length = 150,unique = true)
+    private String email;
+
+    @Column(nullable = false,length = 10)
+    private String ramal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 30)
+    private EspecialidadeTipo especialidade;
+
+    @Column(name = "criado_em",nullable = false,updatable = false)
+    @CreatedDate
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em",nullable = false)
+    @LastModifiedDate
+    private LocalDateTime atualizadoEm;
 }
+
