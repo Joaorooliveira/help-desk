e4c136507b391a106e0cf597a260ee7642652207
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-06 20:26:40 -0300

    feat: adiciona tecnico atualizar dto

--- src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/dto/FuncionarioAtualizarDTO.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/dto/FuncionarioAtualizarDTO.java
@@ -2,14 +2,22 @@ package dev.joaorooliveira.help_desk.domain.funcionario.dto;
 
 import dev.joaorooliveira.help_desk.domain.funcionario.Funcionario;
 import dev.joaorooliveira.help_desk.domain.funcionario.SetorTipo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record FuncionarioAtualizarDTO(
 
-    String nome,
-    String email,
-    String ramal,
-    SetorTipo setor
+        String nome,
 
+        @Email(message = "O email do funcionário deve ser válido")
+        String email,
+
+        @Size(max = 10)
+        String ramal,
+
+        SetorTipo setor
 ) {
 
     public void preencher(Funcionario funcionario) {
---
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/dto/TecnicoAtualizarDTO.java
@@ -0,0 +1,38 @@
+package dev.joaorooliveira.help_desk.domain.tecnico.dto;
+
+import dev.joaorooliveira.help_desk.domain.tecnico.EspecialidadeTipo;
+import dev.joaorooliveira.help_desk.domain.tecnico.Tecnico;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TecnicoAtualizarDTO(
+
+        @Size(max = 150,message = "Nome nao pode ter mais de 150 caracteres")
+        String nome,
+
+        @Email(message = "Email invalido")
+        String email,
+
+        @Size(max = 10,message = "Ramal nao pode ter mais de 10 caracteres")
+        String ramal,
+
+        EspecialidadeTipo especialidade
+) {
+
+    public void preencher(Tecnico tecnico) {
+        if (nome != null) {
+            tecnico.setNome(nome);
+        }
+        if (email != null) {
+            tecnico.setEmail(email);
+        }
+        if (ramal != null) {
+            tecnico.setRamal(ramal);
+        }
+        if (especialidade != null) {
+            tecnico.setEspecialidade(especialidade);
+        }
+    }
+}
