bb6ab7bb396d692929f42c733325b3f1350c86d4
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-06 19:58:42 -0300

    feat: adiciona funcionario response dto

--- src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/dto/FuncionarioResponseDTO.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/dto/FuncionarioResponseDTO.java
@@ -1,4 +1,23 @@
 package dev.joaorooliveira.help_desk.domain.funcionario.dto;
 
-public record FuncionarioResponseDTO() {
+import dev.joaorooliveira.help_desk.domain.funcionario.Funcionario;
+import dev.joaorooliveira.help_desk.domain.funcionario.SetorTipo;
+
+public record FuncionarioResponseDTO(
+        Long id,
+        String nome,
+        String email,
+        String ramal,
+        SetorTipo setor
+) {
+
+    public static FuncionarioResponseDTO fromEntity(Funcionario funcionario){
+        return new FuncionarioResponseDTO(
+                funcionario.getId(),
+                funcionario.getNome(),
+                funcionario.getEmail(),
+                funcionario.getRamal(),
+                funcionario.getSetor()
+        );
+    }
 }
