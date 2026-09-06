6c9cc94a0975375a790dc88bfa72e614d4bdd1d1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-06 19:40:35 -0300

    feat: adiciona enum setor tipo

---
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/SetorTipo.java
@@ -0,0 +1,12 @@
+package dev.joaorooliveira.help_desk.domain.funcionario;
+
+public enum SetorTipo {
+    TI,
+    RH,
+    FINANCEIRO,
+    MARKETING,
+    VENDAS,
+    ADMINISTRATIVO,
+    JURIDICO,
+    COMERCIAL
+}
