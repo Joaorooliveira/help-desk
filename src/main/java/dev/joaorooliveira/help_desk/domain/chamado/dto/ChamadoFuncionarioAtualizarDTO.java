d02e3565c403981a74ffedaf161e856036c7f3bf
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 22:54:48 -0300

    feat(chamado-funcionario-atualizar-dto): adiciona validacao para titulo

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/dto/ChamadoFuncionarioAtualizarDTO.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/dto/ChamadoFuncionarioAtualizarDTO.java
@@ -3,9 +3,11 @@ package dev.joaorooliveira.help_desk.domain.chamado.dto;
 import dev.joaorooliveira.help_desk.domain.chamado.Chamado;
 import dev.joaorooliveira.help_desk.domain.chamado.enums.CategoriaTipo;
 import dev.joaorooliveira.help_desk.domain.chamado.enums.PrioridadeTipo;
+import jakarta.validation.constraints.Size;
 
 public record ChamadoFuncionarioAtualizarDTO(
 
+        @Size(max = 200, message = "O título do chamado deve ter no máximo 200 caracteres")
         String titulo,
         String descricao,
         CategoriaTipo categoria,
