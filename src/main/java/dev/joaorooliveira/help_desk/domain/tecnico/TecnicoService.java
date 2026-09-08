5e5ec53337b9a33366c00e652b3957e2cd784ddd
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 23:54:14 -0300

    feat(tecnico-service): adiciona metodo para atualizar tecnico

--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
@@ -1,5 +1,6 @@
 package dev.joaorooliveira.help_desk.domain.tecnico;
 
+import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoAtualizarDTO;
 import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoRequestDTO;
 import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoResponseDTO;
 import dev.joaorooliveira.help_desk.infra.exception.EntidadeNaoEncontradaException;
@@ -27,11 +28,20 @@ public class TecnicoService {
         return TecnicoResponseDTO.fromEntity(tecnico);
     }
 
+    // Lembrar de validar se o tecnico nao tem chamados associados antes de deletar
     @Transactional
     public void deletarTecnico(Long id) {
         if (!tecnicoRepository.existsById(id)) {
             throw new EntidadeNaoEncontradaException("Técnico não encontrado com o ID: " + id);
         }
         tecnicoRepository.deleteById(id);
+    }
+
+    @Transactional
+    public TecnicoResponseDTO atualizarTecnico(Long id, TecnicoAtualizarDTO tecnicoAtualizarDTO) {
+        Tecnico tecnico = tecnicoRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Técnico não encontrado com o ID: " + id));
+        tecnicoAtualizarDTO.preencher(tecnico);
+        return TecnicoResponseDTO.fromEntity(tecnico);
     }
 }
