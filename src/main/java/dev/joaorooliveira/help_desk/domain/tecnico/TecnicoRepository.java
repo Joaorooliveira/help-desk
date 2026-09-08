ac947d913ac6ac9e5720f9d2fe50b4b0c03e2402
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 23:50:57 -0300

    feat(tecnico-service): adiciona metodo para buscar tecnico pelo id

--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoRepository.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoRepository.java
@@ -3,5 +3,5 @@ package dev.joaorooliveira.help_desk.domain.tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TecnicoRepository extends JpaRepository<Tecnico,Integer>, JpaSpecificationExecutor<Tecnico> {
+public interface TecnicoRepository extends JpaRepository<Tecnico,Long>, JpaSpecificationExecutor<Tecnico> {
 }
--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
@@ -2,6 +2,7 @@ package dev.joaorooliveira.help_desk.domain.tecnico;
 
 import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoRequestDTO;
 import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoResponseDTO;
+import dev.joaorooliveira.help_desk.infra.exception.EntidadeNaoEncontradaException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,12 @@ public class TecnicoService {
     @Transactional
     public TecnicoResponseDTO salvarTecnico(TecnicoRequestDTO tecnicoRequestDTO) {
         Tecnico tecnico = tecnicoRepository.save(tecnicoRequestDTO.toEntity());
+        return TecnicoResponseDTO.fromEntity(tecnico);
+    }
+
+    public TecnicoResponseDTO buscarTecnicoPorId(Long id) {
+        Tecnico tecnico = tecnicoRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Técnico não encontrado com o ID: " + id));
         return TecnicoResponseDTO.fromEntity(tecnico);
     }
 }
