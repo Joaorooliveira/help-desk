be6e791d761cae7ff23b62695591fd48dd3063e1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 22:51:12 -0300

    feat: adiciona funcionario service

---
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioService.java
@@ -0,0 +1,13 @@
+package dev.joaorooliveira.help_desk.domain.funcionario;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FuncionarioService {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+}
