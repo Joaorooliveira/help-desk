9253fddd069d09f1529b2765f0aa66e380f1cff3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 23:26:27 -0300

    feat(funcionario-service): adiciona endpoint para salvar funcionario

--- src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioController.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioController.java
@@ -1,7 +1,16 @@
 package dev.joaorooliveira.help_desk.domain.funcionario;
 
+import dev.joaorooliveira.help_desk.domain.funcionario.dto.FuncionarioRequestDTO;
+import dev.joaorooliveira.help_desk.domain.funcionario.dto.FuncionarioResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/funcionarios")
@@ -11,6 +20,20 @@ public class FuncionarioController {
 
     public FuncionarioController(FuncionarioService funcionarioService) {
         this.funcionarioService = funcionarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FuncionarioResponseDTO> salvar(
+            @RequestBody @Valid FuncionarioRequestDTO funcionarioRequestDTO) {
+
+        FuncionarioResponseDTO funcionarioResponseDTO = funcionarioService.salvarFuncionario(funcionarioRequestDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(funcionarioResponseDTO.id())
+                .toUri();
+        return ResponseEntity.created(location).body(funcionarioResponseDTO);
     }
 
 
