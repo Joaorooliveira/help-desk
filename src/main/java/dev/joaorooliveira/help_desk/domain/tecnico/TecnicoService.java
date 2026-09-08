7d3c47b4583645fbdff162bdc7795b485c8ec4ee
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 23:47:42 -0300

    feat(tecnico-service): adiciona metodo para salvar tecnico

--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoService.java
@@ -1,6 +1,9 @@
 package dev.joaorooliveira.help_desk.domain.tecnico;
 
+import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoRequestDTO;
+import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoResponseDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TecnicoService {
@@ -11,5 +14,9 @@ public class TecnicoService {
         this.tecnicoRepository = tecnicoRepository;
     }
 
-
+    @Transactional
+    public TecnicoResponseDTO salvarTecnico(TecnicoRequestDTO tecnicoRequestDTO) {
+        Tecnico tecnico = tecnicoRepository.save(tecnicoRequestDTO.toEntity());
+        return TecnicoResponseDTO.fromEntity(tecnico);
+    }
 }
