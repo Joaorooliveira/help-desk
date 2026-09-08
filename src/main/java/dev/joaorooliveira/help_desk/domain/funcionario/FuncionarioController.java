749cb663dfc019df6d892ae8216aebf37f8838ee
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 23:23:14 -0300

    feat: adiciona funcionario controller

---
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioController.java
@@ -0,0 +1,17 @@
+package dev.joaorooliveira.help_desk.domain.funcionario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    private final FuncionarioService funcionarioService;
+
+    public FuncionarioController(FuncionarioService funcionarioService) {
+        this.funcionarioService = funcionarioService;
+    }
+
+
+}
