4a415301b82f93ec13cc444bdce0ffe7112075f9
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-08 00:05:11 -0300

    feat(tecnico-controller): adiciona endpoint para salvar tecnico

--- src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoController.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/tecnico/TecnicoController.java
@@ -1,7 +1,14 @@
 package dev.joaorooliveira.help_desk.domain.tecnico;
 
+import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoRequestDTO;
+import dev.joaorooliveira.help_desk.domain.tecnico.dto.TecnicoResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/tecnicos")
@@ -13,5 +20,16 @@ public class TecnicoController {
         this.tecnicoService = tecnicoService;
     }
 
+    @PostMapping
+    public ResponseEntity<TecnicoResponseDTO> salvar(TecnicoRequestDTO tecnicoRequestDTO) {
+        TecnicoResponseDTO tecnicoResponseDTO = tecnicoService.salvarTecnico(tecnicoRequestDTO);
 
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(tecnicoResponseDTO.id())
+                .toUri();
+        return ResponseEntity.created(location).body(tecnicoResponseDTO);
+    }
 }
