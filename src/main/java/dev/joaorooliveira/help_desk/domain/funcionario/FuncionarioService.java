003ba3e4c89d4dd0ea8aca5625171cae175f8c12
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-07 22:57:29 -0300

    feat(funcionario-service): adiciona metodo para buscar funcionario pelo id

--- src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioService.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/funcionario/FuncionarioService.java
@@ -2,6 +2,7 @@ package dev.joaorooliveira.help_desk.domain.funcionario;
 
 import dev.joaorooliveira.help_desk.domain.funcionario.dto.FuncionarioRequestDTO;
 import dev.joaorooliveira.help_desk.domain.funcionario.dto.FuncionarioResponseDTO;
+import dev.joaorooliveira.help_desk.infra.exception.EntidadeNaoEncontradaException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +20,13 @@ public class FuncionarioService {
         Funcionario funcionario = funcionarioRepository.save(funcionarioRequestDTO.toEntity());
         return FuncionarioResponseDTO.fromEntity(funcionario);
     }
+
+    public FuncionarioResponseDTO buscarFuncionarioPorId(Long id) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Funcionário não encontrado com o ID: " + id));
+        return FuncionarioResponseDTO.fromEntity(funcionario);
+    }
+
+
 
 }
