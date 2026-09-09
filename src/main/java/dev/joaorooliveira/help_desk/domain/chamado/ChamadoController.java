baa5d56784a0d668689d1b86dafe3e2072e34049
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-08 22:35:54 -0300

    feat(chamado-controller): adiciona endpoint para salvar chamado

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/ChamadoController.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/ChamadoController.java
@@ -1,7 +1,16 @@
 package dev.joaorooliveira.help_desk.domain.chamado;
 
+import dev.joaorooliveira.help_desk.domain.chamado.dto.ChamadoFuncionarioResponseDTO;
+import dev.joaorooliveira.help_desk.domain.chamado.dto.ChamadoRequestDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/chamados")
@@ -13,6 +22,16 @@ public class ChamadoController {
         this.chamadoService = chamadoService;
     }
 
+    @PostMapping
+    public ResponseEntity<ChamadoFuncionarioResponseDTO> salvarChamado(@RequestBody @Valid ChamadoRequestDTO chamadoRequestDTO) {
+        ChamadoFuncionarioResponseDTO responseDTO = chamadoService.salvarChamado(chamadoRequestDTO);
 
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDTO.id())
+                .toUri();
+        return ResponseEntity.created(location).body(responseDTO);
+    }
 
 }
