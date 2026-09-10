8b02fc7183fd590aaf32a79984ab632cba70e364
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-09 21:50:13 -0300

    feat(concluir-chamado-dto): adiciona atributo para id do tecnico

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/dto/ConcluirChamadoDTO.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/dto/ConcluirChamadoDTO.java
@@ -1,10 +1,13 @@
 package dev.joaorooliveira.help_desk.domain.chamado.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ConcluirChamadoDTO(
 
         @NotBlank(message = "A solução do chamado é obrigatória")
-        String solucao
+        String solucao,
+        @NotNull(message = "O id do tecnico é obrigatório")
+        Long idTecnico
 ) {
 }
