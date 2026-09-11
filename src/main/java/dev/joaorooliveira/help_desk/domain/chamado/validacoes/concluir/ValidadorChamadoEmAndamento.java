8955f07c9b878c030426dd8ba55398e762f03e05
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-10 22:49:13 -0300

    feat: adiciona validador chamado em andamento

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/validacoes/concluir/ValidadorChamadoEmAndamento.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/validacoes/concluir/ValidadorChamadoEmAndamento.java
@@ -1,4 +1,17 @@
 package dev.joaorooliveira.help_desk.domain.chamado.validacoes.concluir;
 
-public class ValidadorChamadoEmAndamento {
+import dev.joaorooliveira.help_desk.domain.chamado.Chamado;
+import dev.joaorooliveira.help_desk.domain.chamado.enums.StatusTipo;
+import dev.joaorooliveira.help_desk.domain.tecnico.Tecnico;
+import dev.joaorooliveira.help_desk.infra.exception.RegraNegocioException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorChamadoEmAndamento implements ValidadorConcluirChamado{
+    @Override
+    public void validar(Chamado chamado, Tecnico tecnico) {
+        if(chamado.getStatus()!= StatusTipo.EM_ANDAMENTO){
+            throw new RegraNegocioException("Chamado precisa estar em andamento para concluir");
+        }
+    }
 }
