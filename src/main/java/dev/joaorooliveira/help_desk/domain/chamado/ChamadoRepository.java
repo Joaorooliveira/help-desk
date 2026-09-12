0c7846dacec1108405b427609671e153a5685c0d
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-11 22:19:37 -0300

    feat(chamado-repository): adiciona metodo para verificar se ja nao tem email cadastrado

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/ChamadoRepository.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/ChamadoRepository.java
@@ -1,5 +1,7 @@
 package dev.joaorooliveira.help_desk.domain.chamado;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,4 +9,6 @@ public interface ChamadoRepository extends JpaRepository<Chamado, Long> , JpaSpe
     boolean existsByFuncionarioId(Long funcionarioId);
 
     boolean existsByTecnicoId(Long idTecnico);
+
+    boolean existsByEmail(String email);
 }
