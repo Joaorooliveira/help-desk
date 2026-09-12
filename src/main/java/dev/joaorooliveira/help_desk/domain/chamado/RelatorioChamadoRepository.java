f232e82af4d0a08e5ffc2ff3e083627fa689438b
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-11 23:00:50 -0300

    feat(relatorio-chamado-repository): adiciona metodo para buscar quantidade de chamados por status

--- src/main/java/dev/joaorooliveira/help_desk/domain/chamado/RelatorioChamadoRepository.java
+++ src/main/java/dev/joaorooliveira/help_desk/domain/chamado/RelatorioChamadoRepository.java
@@ -1,6 +1,7 @@
 package dev.joaorooliveira.help_desk.domain.chamado;
 
 import dev.joaorooliveira.help_desk.projection.ChamadoPorPrioridadeProjection;
+import dev.joaorooliveira.help_desk.projection.ChamadoPorStatusProjection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
@@ -11,12 +12,26 @@ public interface RelatorioChamadoRepository extends Repository<Chamado, Long> {
 
     @Query(
             value = """
-                    SELECT prioridade, COUNT(*) AS total
+                    SELECT
+                        prioridade,
+                        COUNT(*) AS total
                     FROM chamado
                     GROUP BY prioridade
                     """,
             nativeQuery = true
     )
     List<ChamadoPorPrioridadeProjection> chamadoPorPrioridade();
+
+    @Query(
+            value = """
+                     SELECT
+                        status,
+                        COUNT(*) AS total
+                     FROM chamado
+                     GROUP BY status;
+                    """,
+            nativeQuery = true
+    )
+    List<ChamadoPorStatusProjection> chamadoPorStatus();
 
 }
