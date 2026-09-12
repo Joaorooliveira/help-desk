a70734ff99d2cc2272f9e8345ded5bddfee41563
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-11 23:01:28 -0300

    feat: adiciona chamado por status projection

--- src/main/java/dev/joaorooliveira/help_desk/projection/ChamadoPorStatusProjection.java
+++ src/main/java/dev/joaorooliveira/help_desk/projection/ChamadoPorStatusProjection.java
@@ -1,4 +1,12 @@
 package dev.joaorooliveira.help_desk.projection;
 
+import dev.joaorooliveira.help_desk.domain.chamado.enums.StatusTipo;
+
+import java.math.BigDecimal;
+
 public interface ChamadoPorStatusProjection {
+
+    StatusTipo status();
+    BigDecimal total();
+
 }
