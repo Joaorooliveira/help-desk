5a13e73135b6d5df606128ac949cfb900ca3ec49
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-09-13 17:11:41 -0300

    feat: adiciona chamado por categoria projection

---
+++ src/main/java/dev/joaorooliveira/help_desk/projection/ChamadoPorCategoriaProjection.java
@@ -0,0 +1,10 @@
+package dev.joaorooliveira.help_desk.projection;
+
+import java.math.BigDecimal;
+
+public interface ChamadoPorCategoriaProjection {
+
+    String getCategoria();
+    BigDecimal total();
+
+}
